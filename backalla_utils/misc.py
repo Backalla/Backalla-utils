681326989330d508230150e7db498f8a6098aafe
Tushar Pawar <tusharvijaypawar@gmail.com>
2018-07-13 10:49:03 +0530

    Tensorflow, aws and some misc utils added

--- backalla_utils/misc.py
+++ backalla_utils/misc.py
@@ -2,6 +2,36 @@ import sys
 from itertools import chain
 import os
 from glob import glob
+import tarfile
+import os
+
+
+def create_checkpoint_tar(save_path):
+    """
+    Create a tar archive of all checkpoint files for a particular checkpoint
+    :param save_path: Path to the checkpoints
+    :return: Path to the created tar file
+    """
+    checkpoint_suffixes = [".data-00000-of-00001", ".index", ".meta"]
+
+    tar_path = save_path + ".tar"
+
+    with tarfile.open(tar_path, 'w') as tar:
+        for suffix in checkpoint_suffixes:
+            checkpoint_file = save_path+suffix
+            tar.add(checkpoint_file, arcname=os.path.basename(checkpoint_file))
+
+    return tar_path
+
+def untar(tar_path,extract_path):
+    """
+    Extract tar file to a destination
+    :param tar_path: Path to the tar file
+    :param extract_path: Path to extract the tar
+    :return: Nothing
+    """
+    with tarfile.open(tar_path) as tar_file:
+        tar_file.extractall(path=extract_path)
 
 
 
